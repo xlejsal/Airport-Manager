6c79cdcc550226eac291cf88b96860070b2c7f51
Vojtěch Lejsal <lejsal.vojtech@gmail.com>
2018-10-28 15:45:36 +0100

    Added javadoc to the FlightDAO interface

--- airport-manager/core/src/main/java/cz/muni/fi/pa165/airportmanager/core/dao/FlightDAO.java
+++ airport-manager/core/src/main/java/cz/muni/fi/pa165/airportmanager/core/dao/FlightDAO.java
@@ -10,11 +10,29 @@ import java.util.List;
  */
 public interface FlightDAO {
 
+    /**
+     * Finds the flight with the specified id in the database.
+     * @param id Flight's id.
+     * @return Returns flight with specified id.
+     */
     FlightPO findById(long id);
 
+    /**
+     * Saves the flight to the database.
+     * @param flight Flight to be saved.
+     * @return Returns the saved flight.
+     */
     FlightPO create(FlightPO flight);
 
+    /**
+     * Deletes the flight from the database.
+     * @param flight Flight to be deleted.
+     */
     void delete(FlightPO flight);
 
+    /**
+     * Finds all flights saved in the database.
+     * @return Returns list of all flights.
+     */
     List<FlightPO> findAll();
 }
