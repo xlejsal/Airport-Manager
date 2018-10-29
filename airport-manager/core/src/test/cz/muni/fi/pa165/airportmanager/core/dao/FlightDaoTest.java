c930a9bd47b25b48858eccde8e6b15af69c6cbdc
Branislav Kotrč <kotrcb@gmail.com>
2018-10-29 16:32:34 +0100

    created test structure

---
+++ airport-manager/core/src/test/cz/muni/fi/pa165/airportmanager/core/dao/FlightDaoTest.java
@@ -0,0 +1,40 @@
+package cz.muni.fi.pa165.airportmanager.core.dao;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author kotrc
+ * Created on 29.10.2018
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class FlightDaoTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void findById() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void create() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void update() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void delete() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void findAll() {
+    }
+}
