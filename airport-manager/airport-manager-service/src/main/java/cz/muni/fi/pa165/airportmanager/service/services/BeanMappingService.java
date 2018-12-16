9f37b035b611db551101286c4744b582c7a35372
Branislav Kotrč <kotrcb@gmail.com>
2018-12-16 14:57:22 +0100

    New Mapper

--- airport-manager/airport-manager-service/src/main/java/cz/muni/fi/pa165/airportmanager/service/services/BeanMappingService.java
+++ airport-manager/airport-manager-service/src/main/java/cz/muni/fi/pa165/airportmanager/service/services/BeanMappingService.java
@@ -3,11 +3,11 @@ package cz.muni.fi.pa165.airportmanager.service.services;
 import java.util.Collection;
 import java.util.List;
 
-import com.github.dozermapper.core.Mapper;
+import org.modelmapper.ModelMapper;
 
 
 public interface BeanMappingService {
     public  <T> List<T> mapTo(Collection<?> objects, Class<T> mapToClass);
     public  <T> T mapTo(Object u, Class<T> mapToClass);
-    public Mapper getMapper();
+    public ModelMapper getMapper();
 }
